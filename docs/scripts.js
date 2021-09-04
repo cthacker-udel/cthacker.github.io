fbefdd08ed0c49a18dc189cb3e5850aaa6d1b249
cthacker-udel <cthacker@udel.edu>
2021-09-04 08:18:54 -0400

    update

--- docs/scripts.js
+++ docs/scripts.js
@@ -1,9 +1,20 @@
 
+let fetch = require('node-fetch');
 
+function modernGenerateRandomName(){
 
-function generateRandomName(){
+    let theVar = fetch("https://random-word-api.herokuapp.com/word?number=2&swear=0",{method: 'GET'}).then(function(response){
 
-    fetch()
+        if(response.ok){
+            return response.text();
+        }
+        else{
+            return new Error('Could not get data, invalid request');
+        }
+    });
 
+    console.log(1);
 
 }
+
+console.log(modernGenerateRandomName());
