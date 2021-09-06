9dc47747107241b6a4028e28b04ff3bd0b21f236
cthacker-udel <cthacker@udel.edu>
2021-09-06 06:42:02 -0400

    update

--- docs/scripts2.js
+++ docs/scripts2.js
@@ -3,3 +3,10 @@
 document.getElementById("row3col1H1").innerHTML = "HELLO WORLD";
 
 document.getElementById("row3col2H1").innerHTML = "THIS IS A TEST";
+
+
+function basicfunction(){
+
+    alert('using this function');
+
+}
